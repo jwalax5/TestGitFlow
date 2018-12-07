ee331c73dfb1a522c52b6fcbb588293f51497d58
jwalax5 <josh90810@gmail.com>
2018-12-07 20:13:44 +0900

    is there delay for vscode to push to git??

--- src/js/SimpleComponent.js
+++ src/js/SimpleComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SimpleComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: "jojojo josh" };
+    this.state = { name: "jojojos josh" };
   }
 
   render() {
